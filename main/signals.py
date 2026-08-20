39d40ab78e6ad11ba87c137cf340804f6a415ac0
Dmitriy <dimagame73ulru@gmail.com>
2026-08-20 18:48:43 +0400

    Beta vers 0.6.7.8.3 (New signals function to update user nickname and avatar)

--- main/signals.py
+++ main/signals.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.signals import user_logged_in
 from .models import UserActivityLog
 from django.utils import timezone
+from django.dispatch import receiver
+from allauth.socialaccount.signals import social_account_updated, social_account_added
 
 def log_user_activity(sender, user, request, **kwargs):
     UserActivityLog.objects.get_or_create(
@@ -9,3 +11,19 @@ def log_user_activity(sender, user, request, **kwargs):
     )
 
 user_logged_in.connect(log_user_activity)
+
+@receiver([social_account_updated, social_account_added])
+def update_discord_avatar_on_login(request, sociallogin, **kwargs):
+    # Проверяем, что обновление пришло именно от Discord провайдера
+    if sociallogin.account.provider == 'discord':
+        # Вытаскиваем свежие JSON-данные, которые только что прислал Discord
+        new_extra_data = sociallogin.account.extra_data
+        
+        # Получаем уже существующий объект аккаунта из базы данных
+        social_account = sociallogin.account
+        
+        # Принудительно обновляем поле extra_data актуальной информацией
+        social_account.extra_data = new_extra_data
+        social_account.save()
+        
+        print(f"[AnimeGuru] Аватарка для {social_account.user.username} успешно обновлена из Discord!")
