13bda1d452284642809217593682265311dc247a
Dmitriy <dimagame73ulru@gmail.com>
2026-09-04 20:27:17 +0400

    Beta vers 0.6.7.8.6.2 (Fix)

--- static/main/js/open-burger.js
+++ static/main/js/open-burger.js
@@ -2,7 +2,7 @@ function OpenBurger() {
     let menu = document.getElementById('burger-menu-block');
 
     if (menu.style.marginLeft == '0px') {
-        menu.style.marginLeft = '200%';
+        menu.style.marginLeft = '100vw';
     } else {
         menu.style.marginLeft = '0px';
     }
